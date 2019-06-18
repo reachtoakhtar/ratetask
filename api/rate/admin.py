40bdfc50ce21b4fc7dff469c53d761afac1219dd
Akhtar <reachtoakhtar@gmail.com>
2019-06-18 09:55:22 +0530

    - Added models for admin view.

--- api/rate/admin.py
+++ api/rate/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from rate.models import Region, Port, Price
+
+__author__ = "akhtar"
+
+
+class AdminMixin(admin.ModelAdmin):
+    list_per_page = 500
+
+
+class RegionAdmin(AdminMixin):
+    list_display = [f.name for f in Region._meta.fields]
+
+
+class PortAdmin(AdminMixin):
+    list_display = [f.name for f in Port._meta.fields]
+
+
+class PriceAdmin(AdminMixin):
+    list_display = [f.name for f in Price._meta.fields]
+
+
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Port, PortAdmin)
+admin.site.register(Price, PriceAdmin)
